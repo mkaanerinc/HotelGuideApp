9733a9fc80eb3cd443f11190f7b8db453f26bd7b
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2024-01-17 20:13:54 +0300

    Added DbContext, Repositories and PersistenceServiceRegistiration

---
+++ Application/Services/Repositories/IReportDetailRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Persistence.Repositories;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Repositories;
+
+public interface IReportDetailRepository : IAsyncRepository<ReportDetail,Guid>, IRepository<ReportDetail,Guid>
+{
+}
